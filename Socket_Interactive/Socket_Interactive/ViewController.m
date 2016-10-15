57cda3e505e8e9a3fc5cdddb2ae7346004ca95cb
zhonghuan <zhonghphuan@hotmail.com>
2016-10-14 21:47:51 -0700

    创建"建立连接"方法

--- Socket_Interactive/Socket_Interactive/ViewController.m
+++ Socket_Interactive/Socket_Interactive/ViewController.m
@@ -7,25 +7,47 @@
 //
 
 #import "ViewController.h"
+#import <arpa/inet.h>
+#import <netinet/in.h>
+#import <sys/socket.h>
+
 
 @interface ViewController ()
 /**
  *  webview浏览器展示
  */
 @property (weak, nonatomic) IBOutlet UIWebView *webview;
-
+@property (nonatomic,assign) int clientSocket;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self socketDemo];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)socketDemo {
+
+    
+}
+
+#pragma mark - 建立连接
+- (void)connectToServer:(NSString *)ip port:(int)port{
+    
+    _clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    struct sockaddr_in addr;
+    /* 填写sockaddr_in结构*/
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(ip.UTF8String);
+    
+    int connectResult = connect(_clientSocket, (const struct sockaddr *)&addr, sizeof(addr));
+    
+    if (connectResult == 0) {
+        NSLog(@"连接成功");
+    }
 }
 
 @end
